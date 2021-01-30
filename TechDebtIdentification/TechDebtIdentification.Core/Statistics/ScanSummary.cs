955c19d01b5750a4d2ce96a166d309a651c4e981
Sam Smith <samsmithnz@gmail.com>
2021-01-29 20:57:28 -0500

    Fix to stats and added new console app

--- TechDebtIdentification/TechDebtIdentification.Core/Statistics/ScanSummary.cs
+++ TechDebtIdentification/TechDebtIdentification.Core/Statistics/ScanSummary.cs
@@ -6,10 +6,35 @@ namespace TechDebtIdentification.Core.Statistics
 {
     public class ScanSummary
     {
+        private List<FrameworkSummary> _frameworkSummary;
+        private List<LanguageSummary> _languageSummary;
+
         public int ProjectCount { get; set; }
         public int FrameworkCount { get; set; }
-        public List<FrameworkSummary> FrameworkSummary { get; set; }
+        public List<FrameworkSummary> FrameworkSummary
+        {
+            get
+            {
+                return _frameworkSummary;
+            }
+            set
+            {
+                _frameworkSummary = value;
+                FrameworkCount = value.Count;
+            }
+        }
         public int LanguageCount { get; set; }
-        public List<LanguageSummary> LanguageSummary { get; set; }
+        public List<LanguageSummary> LanguageSummary
+        {
+            get
+            {
+                return _languageSummary;
+            }
+            set
+            {
+                _languageSummary = value;
+                LanguageCount = value.Count;
+            }
+        }
     }
 }
---
+++ TechDebtIdentification/TechID.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TechDebtIdentification.Core;
+using TechDebtIdentification.Core.Statistics;
+
+namespace TechID.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan span = new TimeSpan();
+            string repoLocation = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\source\repos\SamLearnsAzure";
+            RepoScanner repo = new RepoScanner();
+            ScanSummary scanSummary = repo.ScanRepo(repoLocation);
+
+            //results
+            Console.WriteLine("Processed in " + span.ToString() + " seconds");
+            Console.WriteLine(scanSummary.ProjectCount + " projects found");
+            Console.WriteLine(scanSummary.FrameworkCount + " frameworks found");
+            Console.WriteLine(scanSummary.LanguageCount + " languages found");
+
+        }
+    }
+}
