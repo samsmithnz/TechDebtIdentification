d340b18dad3e44daa56aac704fc6e251dbed42b1
Sam Smith <samsmithnz@gmail.com>
2021-01-31 19:53:46 -0500

    Added release config

--- TechDebtIdentification/TechDebtIdentification.Tests/RepoScannerIntegrationTests.cs
+++ TechDebtIdentification/TechDebtIdentification.Tests/RepoScannerIntegrationTests.cs
@@ -23,13 +23,12 @@ namespace TechDebtIdentification.Tests
             IProgress<int> progress = new Progress<int>();
             CancellationTokenSource tokenSource = new CancellationTokenSource();
             bool includeTotal = true;
-            string rootFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace("\\TechDebtIdentification.Tests\\bin\\Debug\\net5.0", "");
+            string rootFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location).Replace("\\TechDebtIdentification.Tests\\bin\\Debug\\net5.0", "").Replace("\\TechDebtIdentification.Tests\\bin\\Release\\net5.0", "");
 
             //Act
             ScanSummary results = await repoScanner.ScanRepo(progress, tokenSource.Token, rootFolder, includeTotal, null);
 
             //Asset
-            Assert.AreEqual("", rootFolder);
             Assert.AreEqual(5, results.ReposCount);
             Assert.AreEqual(3, results.ProjectCount);
             Assert.AreEqual(3, results.FrameworkSummary.Count);
