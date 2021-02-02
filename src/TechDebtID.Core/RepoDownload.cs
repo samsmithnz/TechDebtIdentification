725f66ab9b480d4c120e80d88ccf6b8ec140be22
Sam Smith <samsmithnz@gmail.com>
2021-02-02 07:28:22 -0500

    Added more samples

--- src/TechDebtID.Core/RepoDownload.cs
+++ src/TechDebtID.Core/RepoDownload.cs
@@ -11,44 +11,44 @@ using TechDebtID.Core.Statistics;
 
 namespace TechDebtID.Core
 {
-    public class RepoDownload
-    {
+    //public class RepoDownload
+    //{
 
-        public async Task<bool> DownloadFiles(string connectionString, string container)
-        {
-            bool connected = await ConnectToFileShare(connectionString, container);
-            if (connected == true)
-            {
-                //Download the git repos to the file system
+    //    public async Task<bool> DownloadFiles(string connectionString, string container)
+    //    {
+    //        bool connected = await ConnectToFileShare(connectionString, container);
+    //        if (connected == true)
+    //        {
+    //            //Download the git repos to the file system
 
-            }
+    //        }
 
-            return true;
-        }
+    //        return true;
+    //    }
 
-        public async Task<bool> ConnectToFileShare(string connectionString, string containerName)
-        {
-            // Parse the connection string for the storage account.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+    //    public async Task<bool> ConnectToFileShare(string connectionString, string containerName)
+    //    {
+    //        // Parse the connection string for the storage account.
+    //        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
 
-            // Create a CloudFileClient object for credentialed access to File storage.
-            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+    //        // Create a CloudFileClient object for credentialed access to File storage.
+    //        CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
 
-            // Get a reference to the file share we created previously.
-            CloudFileShare share = fileClient.GetShareReference(containerName);
+    //        // Get a reference to the file share we created previously.
+    //        CloudFileShare share = fileClient.GetShareReference(containerName);
 
-            // Ensure that the share exists.
-            if (await share.ExistsAsync())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+    //        // Ensure that the share exists.
+    //        if (await share.ExistsAsync())
+    //        {
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            return false;
+    //        }
 
-        }
+    //    }
 
 
-    }
+    //}
 }
